9f42c6648fe786e377a4f43439fa186daa0fa3d2
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-08-28 11:07:38 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable eol-last */
+import './css/style.css';
+
+import './js/app';
+
+// TODO: write your code in app.js
---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable eol-last */
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = 'variable';
+
+// for demonstration purpose only
+export default function demo(value) {
+  return value;
+}
+
+console.log('app.js included');
